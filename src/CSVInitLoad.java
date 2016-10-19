d84338bf28b823dd255007333f6e0ed68cd9e243
Swapnil Sudam Auti <sauti@buffalo.edu>
2016-10-18 22:26:17 -0400

    Created Prototype for CSVInitLoad

---
+++ src/CSVInitLoad.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+/**
+ * Created by SwapnilSudam on 10/18/2016.
+ */
+public class CSVInitLoad {
+
+    ArrayList<ArrayList<Long>> Table;
+
+    public CSVInitLoad(String filePath){
+
+    }
+    public void parseFile(){
+
+    }
+    public ArrayList<Long> fetchColumn(int col){
+
+    }
+    public Long fetchValue(int col, int row){
+
+    }
+    public ArrayList<Long> fetchColumnByRowID(int col, int rowl, int rowh){
+
+    }
+}
