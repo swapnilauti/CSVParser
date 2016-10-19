9a44643375645a2d80f3bb1336ba32410254425e
Swapnil Sudam Auti <sauti@buffalo.edu>
2016-10-18 22:13:41 -0400

    Created Prototype for CSVTable

---
+++ src/CSVTable.java
@@ -0,0 +1,30 @@
+import jitd.*;
+
+import java.util.ArrayList;
+
+/**
+ * Created by SwapnilSudam on 10/18/2016.
+ */
+public class CSVTable {
+    private ArrayList<Cog> cogs;
+    private boolean hasCog[];
+
+    public CSVTable(String filePath){
+
+    }
+
+    private void createCog(int colNo){
+
+    }
+
+    public ArrayList<Long> rangeScan(int colNo, long low, long high){
+
+    }
+
+    public long lookUp(int col, int row){
+
+    }
+    public long[] lookUp(int col, int row1, int row2){
+
+    }
+}
